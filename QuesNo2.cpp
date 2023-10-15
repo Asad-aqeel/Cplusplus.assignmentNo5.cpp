25e5f2e618924319a62ed480773b895d6bede8f4
Asad-aqeel <144030369+Asad-aqeel@users.noreply.github.com>
2023-10-15 02:32:14 -0700

    Update QuesNo2.cpp

--- QuesNo2.cpp
+++ QuesNo2.cpp
@@ -5,16 +5,17 @@ int main()
 {
 
     int A = 3;
+    int sum;
 
     cout << "A\tA+2\tA+4\tA+6" << endl;
 
-    for (int i = 0; i <= 5; i++)
+    for (int i = 1; i <= 5; i++)
     {
-
-        for (int j = 0; j < 4; j++)
+        sum = A * i;
+        for (int j = 0; j < 1; j++)
         {
 
-            cout << A + (i * 3) + (j * 2) << "\t";
+            cout << sum << "\t" << sum + 2 << "\t" << sum + 4 << "\t" << sum + 6 << "\t";
         }
 
         cout << endl;
