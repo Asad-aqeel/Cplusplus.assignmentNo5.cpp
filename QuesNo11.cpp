529121cf5bee8269b69c05857a47d8b7a546e299
Asad-aqeel <144030369+Asad-aqeel@users.noreply.github.com>
2023-10-15 02:35:43 -0700

    Create QuesNo11.cpp

---
+++ QuesNo11.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace  std;
+
+int main() {
+    int product = 2;
+
+    while (true) {
+        cout << product <<endl;
+        product *= 2;
+    }
+
+    return 0;
+}
